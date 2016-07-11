30e23fdce51f109555bd095d8498b18579c8350c
nyanko-sankaku <g1527066@st.t-kougei.ac.jp>
2016-07-11 17:41:12 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,10 +1,1 @@
-<script type="text/javascript"><!--
-function ChangeTab(tabname) {
-   // 全部消す
-   document.getElementById('tab1').style.display = 'none';
-   document.getElementById('tab2').style.display = 'none';
-   document.getElementById('tab3').style.display = 'none';
-   // 指定箇所のみ表示
-   document.getElementById(tabname).style.display = 'block';
-}
-// --></script>
+
